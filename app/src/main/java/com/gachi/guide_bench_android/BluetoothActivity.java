09df4397b7461655a628273f9720eb50920ed15e
sohee lee <shfksthgml@naver.com>
2018-12-07 04:32:08 +0900

    bluetooth 구현 완료

--- app/src/main/java/com/gachi/guide_bench_android/BluetoothActivity.java
+++ app/src/main/java/com/gachi/guide_bench_android/BluetoothActivity.java
@@ -1,32 +1,83 @@
 package com.gachi.guide_bench_android;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.gachi.guide_bench_android.R;
 
-public class BluetoothActivity extends AppCompatActivity {   @Override
+public class BluetoothActivity extends AppCompatActivity {
+    private ImageView img_bluetooth_back;
+   static int REQUEST_ENABLE_BT = 1004;
+    @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_qnadetail);
+    setContentView(R.layout.activity_bluetooth);
 
 
 
-    getDefaultAdapter();
+        checkBlueTooth();
+        Back();
 
 }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode ==1004){
+            if(resultCode == RESULT_OK) {
+                // 블루투스가 활성 상태로 변경됨
+                Toast.makeText(getApplicationContext(),"블루투스가 활성화 되었습니다.",Toast.LENGTH_LONG).show();
+                finish();
+            }
 
-    private void getDefaultAdapter(){
+            else if(resultCode == RESULT_CANCELED) {
+                // 블루투스가 비활성 상태임
+                finish();  //  어플리케이션 종료
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void Back(){
+        img_bluetooth_back = (ImageView) findViewById(R.id.img_bluetooth_back);
+        img_bluetooth_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void checkBlueTooth(){
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(mBluetoothAdapter == null) {
             //장치가 블루투스를 지원하지 않는 경우.
+            finish();
         }
 
         else {
             // 장치가 블루투스를 지원하는 경우.
+            if(!mBluetoothAdapter.isEnabled()) {
+
+                // 블루투스를 지원하지만 비활성 상태인 경우
+                // 블루투스를 활성 상태로 바꾸기 위해 사용자 동의 요첨
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+            }
+            else {
+                // 블루투스를 지원하며 활성 상태인 경우
+                // 페어링된 기기 목록을 보여주고 연결할 장치를 선택.
+            }
+
         }
 
     }
+
 }
--- app/src/main/java/com/gachi/guide_bench_android/fragment/SettingFragment.java
+++ app/src/main/java/com/gachi/guide_bench_android/fragment/SettingFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.gachi.guide_bench_android.BluetoothActivity;
 import com.gachi.guide_bench_android.LoginActivity;
 import com.gachi.guide_bench_android.R;
 
@@ -18,6 +19,7 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class SettingFragment extends Fragment {
 private TextView txt_logout;
+private TextView txt_setting_bluetooth;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,14 @@ private TextView txt_logout;
                              Bundle savedInstanceState) {
         /* Inflate the layout for this fragment */
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
-
+        txt_setting_bluetooth= (TextView) view.findViewById(R.id.txt_setting_bluetooth);
+        txt_setting_bluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), BluetoothActivity.class);
+                startActivity(intent1);
+            }
+        });
         txt_logout= (TextView) view.findViewById(R.id.txt_logout);
         txt_logout.setOnClickListener(new View.OnClickListener() {
             @Override
